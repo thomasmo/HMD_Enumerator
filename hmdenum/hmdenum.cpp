fc2d3a631369f0f671b9bd1f40ba67b28589bce7
thomasmo <thomasmo@mozilla.com>
2019-01-29 18:05:05 -0600

    Detect if an HMD is present/connected to the system

--- hmdenum/hmdenum.cpp
+++ hmdenum/hmdenum.cpp
@@ -7,17 +7,10 @@
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
-	printf("vr::VR_IsRuntimeInstalled -- %u", ::vr::VR_IsRuntimeInstalled());
+    std::cout << "HMD_Enumerator\n"; 
+	printf("vr::VR_IsRuntimeInstalled -- %u\n", ::vr::VR_IsRuntimeInstalled());
+	vr::HmdError eError;
+	::vr::IVRSystem* mVRSystem = ::vr::VR_Init(&eError, ::vr::VRApplication_Scene);
+	bool isConnected = mVRSystem->IsTrackedDeviceConnected(::vr::k_unTrackedDeviceIndex_Hmd);
+	printf("vr::IVRSystem::IsTrackedDeviceConnected -- %u\n", isConnected);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
