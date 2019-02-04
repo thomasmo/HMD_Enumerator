5bbdd72a1f6e5cfd12addaad284735cdb85d9975
thomasmo <thomasmo@mozilla.com>
2019-02-04 11:00:16 -0600

    Try multiple VR_Inits to avoid UI

--- hmdenum/hmdenum.cpp
+++ hmdenum/hmdenum.cpp
@@ -9,8 +9,28 @@ int main()
 {
     std::cout << "HMD_Enumerator\n"; 
 	printf("vr::VR_IsRuntimeInstalled -- %u\n", ::vr::VR_IsRuntimeInstalled());
+	printf("vr::VR_IsHmdPresent -- %u\n", ::vr::VR_IsHmdPresent());
 	vr::HmdError eError;
-	::vr::IVRSystem* mVRSystem = ::vr::VR_Init(&eError, ::vr::VRApplication_Scene);
-	bool isConnected = mVRSystem->IsTrackedDeviceConnected(::vr::k_unTrackedDeviceIndex_Hmd);
-	printf("vr::IVRSystem::IsTrackedDeviceConnected -- %u\n", isConnected);
+	
+	::vr::IVRSystem* mVRSystemU = ::vr::VR_Init(&eError, ::vr::VRApplication_Utility);
+	if (mVRSystemU != nullptr)
+	{
+		bool isConnectedU = mVRSystemU->IsTrackedDeviceConnected(::vr::k_unTrackedDeviceIndex_Hmd);
+		printf("vr::IVRSystem::IsTrackedDeviceConnected -- %u\n", isConnectedU);
+	}
+	else
+	{
+		std::cout << "Failed to create Utility";
+	}
+
+	::vr::IVRSystem* mVRSystemS = ::vr::VR_Init(&eError, ::vr::VRApplication_Scene);
+	if (mVRSystemS != nullptr)
+	{
+		bool isConnectedS = mVRSystemS->IsTrackedDeviceConnected(::vr::k_unTrackedDeviceIndex_Hmd);
+		printf("vr::IVRSystem::IsTrackedDeviceConnected -- %u\n", isConnectedS);
+	}
+	else
+	{
+		std::cout << "Failed to create Scene";
+	}
 }
